bf7fcb773ec78e3a7e96412034b40efbfbd483e7
Uday-20102005 <jayiniudaykiran@gmail.com>
2026-06-09 10:37:43 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
-        "https://analyzr-hti8z4esb-uday-20102005s-projects.vercel.app",
+        "https://analyzr-qcgtixqcn-uday-20102005s-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
